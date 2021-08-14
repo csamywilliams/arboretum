f5136109f56303f9f76fea30710a4d708afc8aef
Amy Williams <csamywilliams@gmail.com>
2021-08-14 10:54:14 +0100

    Added mocked tree data

--- src/js/trees.js
+++ src/js/trees.js
@@ -3,10 +3,12 @@ export const trees = [
 		botanicalName: 'Acer amplum',
 		commonName: 'Broad Maple',
 		category: 'acer',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 	},
 	{
 		botanicalName: 'Acer argutum',
 		commonName: 'Deep-veined Maple',
 		category: 'acer',
+		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
 	},
 ];
