0350031b5cbc5b48b8ee01f748b8f0924a1a9e25
Amy Williams <csamywilliams@gmail.com>
2021-08-15 19:00:21 +0100

    Fixed styling for mobiles, added missing prop-types

--- src/js/components/dashboard/Dashboard.styled.jsx
+++ src/js/components/dashboard/Dashboard.styled.jsx
@@ -2,16 +2,20 @@ import styled from 'styled-components';
 import { devices } from 'utils/devices';
 
 const ContentStyled = styled.div`
-	display: flex;
-	flex-direction: row;
+    display: flex;
+    flex-direction: row;
+
+    @media ${devices.laptop} {
+        flex-direction: column;
+    }
 `;
 
 const AsideStyled = styled.aside`
-	width: 15vw;
+    width: 15vw;
 `;
 
 const MainStyled = styled.main`
-	flex: 1;
+    flex: 1;
 `;
 
 export { ContentStyled, AsideStyled, MainStyled };
