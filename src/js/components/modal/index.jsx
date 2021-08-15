a577dfc9bd98158d228ef673884b0e666831aa82
Amy Williams <csamywilliams@gmail.com>
2021-08-15 15:28:56 +0100

    Removed button from modal

--- src/js/components/modal/index.jsx
+++ src/js/components/modal/index.jsx
@@ -2,66 +2,37 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 
-import Button from 'components/button';
-
 const customStyles = {
-	content: {
-		width: '30vw',
-		height: '50vh',
-		top: '50%',
-		left: '50%',
-		right: 'auto',
-		bottom: 'auto',
-		marginRight: '-50%',
-		transform: 'translate(-50%, -50%)',
-	},
+    content: {
+        width: '30vw',
+        height: '50vh',
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+    },
 };
 
 Modal.setAppElement('#root');
 
-const ModalOverlay = ({
-	children,
-	modalIsOpen,
-	setModalIsOpen,
-	buttonProps,
-}) => {
-	const onClose = () => setModalIsOpen(false);
-
-	const { buttonText, buttonAriaLabel, onButtonClick } = buttonProps;
-
-	const onClick = () => {
-		onButtonClick();
-		onClose();
-	};
-
-	return (
-		<Modal
-			isOpen={modalIsOpen}
-			onRequestClose={onClose}
-			contentLabel="Modal"
-			style={customStyles}
-		>
-			<div>{children}</div>
-			<Button
-				text={buttonText}
-				ariaLabel={buttonAriaLabel}
-				onClick={onClick}
-			/>
-		</Modal>
-	);
+const ModalOverlay = ({ children, modalIsOpen }) => {
+    return (
+        <Modal isOpen={modalIsOpen} contentLabel="Modal" style={customStyles}>
+            <div>{children}</div>
+        </Modal>
+    );
 };
 
 ModalOverlay.defaultProps = {
-	onClick() {},
+    onClick() {},
 };
 
 ModalOverlay.propTypes = {
-	buttonProps: PropTypes.shape({
-		buttonText: PropTypes.string.isRequired,
-		buttonAriaLabel: PropTypes.string.isRequired,
-		onButtonClick: PropTypes.func.isRequired,
-	}).isRequired,
-	onClick: PropTypes.func,
+    children: PropTypes.node.isRequired,
+    modalIsOpen: PropTypes.bool.isRequired,
+    onClick: PropTypes.func,
 };
 
 export default ModalOverlay;
