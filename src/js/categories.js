124b3ddc4ec043f337792fc8c84b99b8d2e8f090
Amy Williams <csamywilliams@gmail.com>
2022-12-15 19:08:45 +0000

    Restart of components

--- src/js/categories.js
+++
@@ -1,9 +0,0 @@
-const categories = {
-    ACER: 'acer',
-    APPLE: 'apple',
-    PLUM: 'plum',
-    MAGNOLIA: 'magnolia',
-    WILLOW: 'willow',
-};
-
-export default categories;
--- src/js/utils/__tests__/transformData.test.js
+++
@@ -1,9 +0,0 @@
-import { groupCategories } from 'utils/transformData';
-
-import { testData } from '__testHelpers__/testData';
-
-describe('groupCategories utils', () => {
-	it('should group all the data into categories in the data', () => {
-		expect(groupCategories(testData)).toEqual(['acer', 'aspen']);
-	});
-});
