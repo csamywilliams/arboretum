57e6ce8e529074e9a2d162083ef0814ca6f19d12
Amy Williams <csamywilliams@gmail.com>
2023-01-01 18:51:19 +0000

    Made Card flip with extra information and added test

--- src/components/card/Card.test.tsx
+++ src/components/card/Card.test.tsx
@@ -1,7 +1,7 @@
 import { screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import Card from "./Card";
 import renderWithTheme from "__testHelpers__/renderWithTheme";
-import Category from "data/categories";
 import mockTrees from "__mocks__/testData";
 
 const [tree] = mockTrees;
@@ -15,5 +15,21 @@ describe("Card", () => {
     expect(screen.getByText(tree.commonName)).toBeInTheDocument();
     expect(screen.getByText(tree.category)).toBeInTheDocument();
     expect(screen.getByText(tree.botanicalName)).toBeInTheDocument();
+  });
+
+  it("should flip back and for", async () => {
+    renderView();
+
+    expect(screen.getByText(tree.commonName)).toBeInTheDocument();
+    expect(screen.getByText(tree.botanicalName)).toBeInTheDocument();
+
+    userEvent.click(screen.getByText(tree.commonName));
+
+    expect(await screen.findByText(tree.description)).toBeInTheDocument();
+
+    userEvent.click(screen.getByText(tree.description));
+
+    expect(await screen.findByText(tree.botanicalName)).toBeInTheDocument();
+    expect(screen.queryByText(tree.description)).not.toBeInTheDocument();
   });
 });
--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,5 +1,5 @@
 import { Tree } from "data/trees";
-import { FC, Suspense } from "react";
+import { FC, Suspense, useState } from "react";
 import CardStyled, { CardImage, CardTag } from "./Card.styled";
 
 const hyphenateWord = (data) => data.replace(/\s+/g, "-").toLowerCase();
@@ -9,20 +9,32 @@ interface Props {
 }
 
 const Card: FC<Props> = ({ tree }: Props) => {
+  const [flip, setFlip] = useState(false);
   const filename = hyphenateWord(tree.botanicalName);
 
+  const onClick = () => setFlip(!flip);
+
   return (
-    <CardStyled>
+    <CardStyled onClick={onClick}>
       <h3>{tree.commonName}</h3>
-      <CardTag>{tree.category}</CardTag>
-      <Suspense fallback={<p>Loading image...</p>}>
-        <CardImage
-          src={`/images/${filename}.png`}
-          alt={`Image of ${tree.commonName}`}
-          onError={(e: any) => (e.target.src = "/images/placeholder.png")}
-        />
-      </Suspense>
-      <p>{tree.botanicalName}</p>
+      {!flip && (
+        <>
+          <CardTag>{tree.category}</CardTag>
+          <Suspense fallback={<p>Loading image...</p>}>
+            <CardImage
+              src={`/images/${filename}.png`}
+              alt={`Image of ${tree.commonName}`}
+              onError={(e: any) => (e.target.src = "/images/placeholder.png")}
+            />
+          </Suspense>
+          <p>{tree.botanicalName}</p>
+        </>
+      )}
+      {flip && (
+        <>
+          <p>{tree.description}</p>
+        </>
+      )}
     </CardStyled>
   );
 };
